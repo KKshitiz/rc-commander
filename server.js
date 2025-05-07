9fd97321a74f81d89fe3a171bcc72bdf3607c3d1
Kshitiz Kamal <skilite007@gmail.com>
2025-05-07 22:24:51 +0530

    fix server script

--- server.js
+++ server.js
@@ -10,41 +10,53 @@ const wss = new WebSocket.Server({ server });
 app.use(express.static("public"));
 
 wss.on("connection", (ws) => {
-  console.log("New WebSocket connection");
+  const clientIp = ws._socket.remoteAddress;
+  const clientPort = ws._socket.remotePort;
+  const userAgent = ws.protocol || "Unknown";
+  console.log(
+    `New WebSocket connection from ${clientIp}:${clientPort} with User-Agent: ${userAgent}`
+  );
+  console.log(`Total active connections: ${wss.clients.size}`);
 
   ws.on("message", (message) => {
     try {
       const data = JSON.parse(message);
       console.log("Received:", data);
-
-      // Process different actions
-      if (data.action === "move") {
-        console.log(`Movement: ${data.direction}`);
-        // Here you would add code to control the RC car
-        // based on the direction received
-
-        switch (data.direction) {
-          case "up":
-            // Code to move forward
-            console.log("Moving forward");
-            break;
-          case "down":
-            // Code to move backward
-            console.log("Moving backward");
-            break;
-          case "left":
-            // Code to turn left
-            console.log("Turning left");
-            break;
-          case "right":
-            // Code to turn right
-            console.log("Turning right");
-            break;
+      // Send the received data to all clients except the sender
+      wss.clients.forEach((client) => {
+        // Check if client is open and not the sender
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(data));
         }
-      } else if (data.action === "stop") {
-        // Code to stop the RC car
-        console.log("Stopping movement");
-      }
+      });
+      // Process different actions
+      //   if (data.action === "move") {
+      //     console.log(`Movement: ${data.direction}`);
+      //     // Here you would add code to control the RC car
+      //     // based on the direction received
+
+      //     switch (data.direction) {
+      //       case "up":
+      //         // Code to move forward
+      //         console.log("Moving forward");
+      //         break;
+      //       case "down":
+      //         // Code to move backward
+      //         console.log("Moving backward");
+      //         break;
+      //       case "left":
+      //         // Code to turn left
+      //         console.log("Turning left");
+      //         break;
+      //       case "right":
+      //         // Code to turn right
+      //         console.log("Turning right");
+      //         break;
+      //     }
+      //   } else if (data.action === "stop") {
+      //     // Code to stop the RC car
+      //     console.log("Stopping movement");
+      //   }
     } catch (error) {
       console.error("Error processing message:", error);
     }
